b7f33715c3cc163706c2158af062866f9d426452
bradly0cjw <57744542+bradly0cjw@users.noreply.github.com>
2024-06-20 10:43:26 +0800

    [upload] shit code
    Signed-off-by: bradly0cjw <57744542+bradly0cjw@users.noreply.github.com>

--- HW2-PL/program2.cbl
+++ HW2-PL/program2.cbl
@@ -13,33 +13,109 @@ FILE-CONTROL.
 
 DATA DIVISION.
 FILE SECTION.
-FD StudentMainFile.
-01 StudentMainRecord.
-    05 StudentID PIC X(10).
-    05 PaymentType PIC X(10).
-FD FeesFile.
-01 FeesRecord.
-    05 PaymentType PIC X(10).
-    05 Amount PIC 9(5)V99.
-FD StudentPaymentFile.
-01 StudentPaymentRecord.
-    05 StudentID PIC X(10).
-    05 Amount PIC 9(5)V99.
+FD  StudentMainFile.
+01  StudentMainRecord.
+    05 StudentID PIC X(9).
+    05 FILLER PIC X.
+     05 PaymentType PIC X(1).
+    05 FILLER PIC X.
+    05 Studentname PIC X(10).
+FD  FeesFile.
+01  FeesRecord.
+    05 PaymentType PIC X(1).
+    05 FILLER PIC X.
+    05 FILLER PIC X.
+    05 Amount PIC 9(5).
+    05 FILLER PIC X.
 
+FD  StudentPaymentFile.
+01  StudentPaymentRecord.
+    05 StudentID-Payment PIC X(9).
+       05 FILLER PIC X.
+       05 FILLER PIC X.
+    05 Amount-Payment PIC 9(5).
+    05 FILLER PIC X.
+WORKING-STORAGE SECTION.
+01  CombinedDataRecord.
+    05 CR-StudentID-Output PIC X(10).
+    05 CR-StudentName-Output PIC X(7).
+    05 CR-PaymentType-Output PIC X(1).
+    05 CR-FeesAmount-Output PIC 9(5).
+    05 CR-PaymentAmount-Output PIC 9(5).
+    05 CR-Short PIC 9(5).
+01 TotalReceived PIC 9(7) VALUE ZERO.
+01 EOF1 PIC X(1) VALUE 'N'.
+01 EOF2 PIC X(1) VALUE 'N'.
+01 EOF3 PIC X(1) VALUE 'N'.
+01 EOF4 PIC X(1) VALUE 'N'.
+01 flag PIC X(1) VALUE 'N'.
 PROCEDURE DIVISION.
     OPEN INPUT StudentMainFile
-    OPEN INPUT FeesFile
-    OPEN INPUT StudentPaymentFile
     READ StudentMainFile
-        AT END DISPLAY "End of Student Main file."
-    END-READ
-    READ FeesFile
-        AT END DISPLAY "End of Fees file."
-    END-READ
-    READ StudentPaymentFile
-        AT END DISPLAY "End of Student Payment file."
-    END-READ
+       PERFORM UNTIL EOF1 = 'Y'
+           READ StudentMainFile
+               AT END MOVE 'Y' TO EOF1
+               NOT AT END 
+                           MOVE StudentID OF StudentMainRecord TO CR-StudentID-Output
+                           MOVE PaymentType OF StudentMainRecord TO CR-PaymentType-Output
+                           MOVE StudentName OF StudentMainRecord TO CR-StudentName-Output
+                           MOVE 'N' TO EOF2
+                           OPEN INPUT FeesFile
+                           PERFORM UNTIL EOF2 = 'Y'
+                               READ FeesFile
+                                   AT END MOVE 'Y' TO EOF2
+                                   NOT AT END 
+      *>                             display "FeesFile PaymentType: " PaymentType OF FeesRecord
+      *>                                display "CR-PaymentType-Output: " CR-PaymentType-Output
+                                   IF PaymentType OF FeesRecord = CR-PaymentType-Output
+      *>                             display "FeesFile Amount: " Amount OF FeesRecord
+      *>                             display "CR-PaymentType-Output: " PaymentType OF FeesRecord
+                                               MOVE Amount OF FeesRecord TO CR-FeesAmount-Output
+                                               MOVE 'Y' TO EOF2
+                                   END-IF
+                                   END-READ
+                           END-PERFORM
+                           Close FeesFile
+                            OPEN INPUT StudentPaymentFile
+                           MOVE 'N' TO EOF3
+                           MOVE 'N' TO flag
+                          PERFORM UNTIL EOF3 = 'Y'
+                              READ StudentPaymentFile
+                                  AT END MOVE 'Y' TO EOF3
+                                  NOT AT END 
+      *>                            display "StudentPaymentFile StudentID-Payment: " StudentID-Payment OF StudentPaymentRecord
+      *>                              display "Amount-Payment: " Amount-Payment OF StudentPaymentRecord
+                                  IF StudentID-Payment OF StudentPaymentRecord = CR-StudentID-Output
+                                              MOVE Amount-Payment OF StudentPaymentRecord TO CR-PaymentAmount-Output
+                                              
+      *>                                        display "@@@@CR-PaymentAmount-Output: " CR-PaymentAmount-Output
+      *>                            display "StudentID-Payment: " StudentID-Payment OF StudentPaymentRecord
+
+                                                MOVE 'Y' TO EOF3
+                                                MOVE 'Y' TO flag
+                                  END-IF
+                              END-READ
+                          END-PERFORM
+                          IF flag = 'N'
+                              MOVE 0 TO CR-PaymentAmount-Output
+                            END-IF
+                            Close StudentPaymentFile
+                           add CR-PaymentAmount-Output to TotalReceived
+                           Move 0 to CR-Short
+                           IF CR-FeesAmount-Output > CR-PaymentAmount-Output
+                               DISPLAY "Student " CR-StudentName-Output " did not pay the required fees."
+                               COMPUTE CR-Short = CR-PaymentAmount-Output - CR-FeesAmount-Output
+                               display  "Amount short: " CR-Short
+                           END-IF
+      *>                      display CR-StudentID-Output "@" CR-StudentName-Output "@" CR-PaymentType-Output "@" CR-FeesAmount-Output "@" CR-PaymentAmount-Output "@" CR-Short
+      *>                     DISplay "\n"
+
+           END-READ
+       END-PERFORM
+      
+
+    DISPLAY "Total amount received from students: " TotalReceived
+
     CLOSE StudentMainFile
-    CLOSE FeesFile
-    CLOSE StudentPaymentFile
+
     STOP RUN.
